81ee4b3037fbd834d6eeac5d34a859f983ebd94c
SabaFadhl <sabafadl98@gmail.com>
2025-03-22 13:43:54 +0300

    feat: impelemnet mcontract code

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -4,13 +4,8 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
-    // Constructor to initialize the token
-    // Name: "AssignmentToken", Symbol: "AST"
     constructor() ERC20("AssignmentToken", "AST") {}
 
-    // Function to mint tokens
-    // - External visibility
-    // - Takes `to` address and `amount` as parameters
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }

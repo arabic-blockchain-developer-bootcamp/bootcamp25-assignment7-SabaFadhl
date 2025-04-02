862618f6480cea095a9d68e0225877173a20fd09
SabaFadhl <sabafadl98@gmail.com>
2025-04-02 21:47:36 +0300

    Feat: test faild install open zipline

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
-    constructor() ERC20("AssignmentToken", "AST") {}
+    constructor() ERC20("Saba", "SBA") {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
